7546a03813889e7d2c8013df0c156ad039dd79b2
James <jamesmotes@gmail.com>
2019-04-12 15:52:51 -0500

    Uncommenting description parameter in robots launch file.

---
+++ build/multi_robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/james/multi_robot_ws/src/multi_robot
+# Build directory: /home/james/multi_robot_ws/build/multi_robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
